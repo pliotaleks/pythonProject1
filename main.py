92840d6bcf4a655a11ddabc1533eae4fb07eafc8
Aleksandr_Pykhov <pilotaleks77@mail.ru>
2023-04-02 21:47:27 +0300

    last changes in main

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import funcs
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-m
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-from funcs.py import plus
-
-plus(3,1)
-from funcs.py import minus
-minus(3,1)
